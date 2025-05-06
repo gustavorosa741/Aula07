9a10014a430448e262aa779fe799a4ffabea6b74
Gustavo Claudio Rosa <zegu11@hotmail.com>
2025-05-05 21:25:37 -0300

    correção

--- Documents/Aula/13.py
+++ Documents/Aula/13.py
@@ -5,11 +5,11 @@ estudante = str(input("Você é estudante? "))
 final = []
 
 if idade <=12:
-    print(f"Você tem 50% de desconto, totaliznado {valor * 0.5}")
+    print(f"Você tem 50% de desconto, totaliznado R${valor - valor * 0.5}")
 elif idade >= 60:
-    print(f"Você tem 30% de desconto, totalizando {valor * 0.3}")
+    print(f"Você tem 30% de desconto, totalizando R${valor - valor * 0.3}")
 elif estudante == "sim":
-    print(f"Você tem 10% de desconto, totalizando: {valor * 0.1}")
+    print(f"Você tem 10% de desconto, totalizando: R${valor - valor * 0.1}")
 
 else:
     print("você não tem desconto.")
